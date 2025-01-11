cda0cc997a32848bb8daea9af3bfd62c8688fc23
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-12 00:50:58 +0530

    add gluestack ui

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,17 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import products from "../assets/products.json";
-import ProductListItem from "../components/ProductListItem";
+import products from "@/assets/products.json";
+import ProductListItem from "@/components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item}>{item.name}</ProductListItem>} />
-  );
+    <Button size="sm" variant="solid" action="positive">
+      <ButtonText>
+        Click Me
+      </ButtonText>
+    </Button>
+  )
+  // return (
+  //   <FlatList data={products} renderItem={({ item }) => <ProductListItem product={item}>{item.name}</ProductListItem>} />
+  // );
 }
