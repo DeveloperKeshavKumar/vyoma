ba88ba93c756b87d08b9eda7c3ba92e718e1d857
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-12 02:47:57 +0530

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { json, urlencoded } from 'express';
+import cors from 'cors';
 import serverless from "serverless-http";
 import router from './routes/index.js';
 
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(urlencoded({ extended: false }));
 app.use(json());
+app.use(cors(
+   {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+   }
+))
 app.use('/', router);
 
 if (process.env.NODE_ENV === 'DEV') {
