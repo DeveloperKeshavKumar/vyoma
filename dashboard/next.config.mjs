530ef38f43cb11b15572b94c033ca3b220fcdbde
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-16 01:28:31 +0530

    try to deploy to genezio #1

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,7 +1,14 @@
 import { withGluestackUI } from "@gluestack/ui-next-adapter";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["nativewind", "react-native-css-interop"]
-};
+  transpilePackages: ["nativewind", "react-native-css-interop"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+cacheHandler: process.env.NODE_ENV === "production" ? "./cache-handler.mjs" : undefined,
+  cacheMaxMemorySize: 0,};
 
 export default withGluestackUI(nextConfig);
